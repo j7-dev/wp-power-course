19976037e1c4fc824ab542114465d3d1b0575234
J7 <j7.dev.gg@outlook.com>
2024-06-26 17:39:38 +0800

    feat: 購物車元件

--- inc/templates/components/button/add-to-cart.php
+++ inc/templates/components/button/add-to-cart.php
@@ -1,18 +1,36 @@
 <?php
 // TODO
 
-$props = $args;
-
-$default_props = [
-	'children' => '加入購物車',
-	'icon'     => '',
-	'href'     => '',
+$default_args = [
+	'product'       => null,
+	'class'         => '',
+	'wrapper_class' => '[&_.added]:hidden',
+	'label'         => '加入購物車',
 ];
 
-$props = \array_merge( $default_props, $props );
+/**
+ * @var array $args
+ */
+$merged_args = \wp_parse_args( $args, $default_args );
 
-$html  = '<a href="?add-to-cart=391" data-quantity="1" class="group product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="391" data-product_sku="" aria-describedby="" rel="nofollow">';
-$html .= self::base( $props );
-$html .= '</a>';
 
-echo $html;
+[
+	'product'       => $product,
+	'class'         => $class,
+	'wrapper_class' => $wrapper_class,
+	'label'         => $label
+] = $merged_args;
+
+$sku  = $product->get_sku();
+$name = $product->get_name();
+
+
+printf(
+	'<div class="%6$s"><a href="?add-to-cart=%1$s" data-quantity="1" class="text-nowrap button product_type_simple add_to_cart_button ajax_add_to_cart %2$s" data-product_id="%1$s" data-product_sku="%3$s" aria-label="Add to cart: “%4$s”" aria-describedby="" rel="nofollow">%5$s</a></div>',
+	$product->get_id(),
+	$class,
+	$sku,
+	$name,
+	$label,
+	$wrapper_class
+);
