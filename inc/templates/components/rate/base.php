9fa0daf910cb89b720e9039eaa4e4c3494a2378e
J7 <j7.dev.gg@outlook.com>
2024-06-26 17:57:10 +0800

    feat: rate 元件

--- inc/templates/components/rate/base.php
+++ inc/templates/components/rate/base.php
@@ -1,5 +1,8 @@
 <?php
-$props = $args;
+
+/**
+ * @var array $args
+ */
 
 $default_props = [
 	'show_before' => false, // 是否顯示前面的文字
@@ -9,7 +12,7 @@ $default_props = [
 	'disabled'    => true, // 是否禁用
 ];
 
-$props = \array_merge( $default_props, $props );
+$props = wp_parse_args( $args, $default_props );
 
 $value       = $props['value'];
 $count       = $props['count'];
@@ -18,8 +21,10 @@ $show_before = $props['show_before'];
 $disabled    = $props['disabled'];
 $rest        = fmod( $value, 1 );
 
-$fill_start_num    = ( (int) $value ) + ( $rest >= 0.8 ? 1 : 0 );
-$half_start_num    = ( $rest > 0.2 && $rest < 0.8 ) ? 1 : 0;
+$fill_start_num = ( (int) $value ) + ( $rest >= 0.8 ? 1 : 0 );
+$half_start_num = ( $rest > 0.2 && $rest < 0.8 ) ? 1 : 0;
+
+
 $outline_start_num = $count - $fill_start_num - $half_start_num;
 $cursor            = $disabled ? 'cursor-default' : '';
 $disabled          = $disabled ? 'disabled' : '';
@@ -28,18 +33,46 @@ $icons_html = '<div class="pc-rating pc-rating-sm pc-rating-half"><input type="r
 for ( $i = 0; $i < $count; $i++ ) {
 	$half_checked = ( $i === ( (int) $fill_start_num ) && ! $half_start_num ) ? 'checked' : '';
 	$full_checked = ( $i === ( (int) $fill_start_num ) && ! ! $half_start_num ) ? 'checked' : '';
-	$icons_html  .= '<input type="radio" name="rating-10" class="bg-yellow-400 pc-mask pc-mask-star-2 pc-mask-half-1 ' . $cursor . '" ' . $full_checked . ' ' . $disabled . ' />';
-	$icons_html  .= '<input type="radio" name="rating-10" class="bg-yellow-400 pc-mask pc-mask-star-2 pc-mask-half-2 ' . $cursor . '" ' . $half_checked . ' ' . $disabled . ' />';
+	$icons_html  .= sprintf(
+		'<input type="radio" name="rating-10" class="bg-yellow-400 pc-mask pc-mask-star-2 pc-mask-half-1 %1$s" %2$s %3$s />',
+		$cursor,
+		$full_checked,
+		$disabled
+	);
+	$icons_html  .= sprintf(
+		'<input type="radio" name="rating-10" class="bg-yellow-400 pc-mask pc-mask-star-2 pc-mask-half-2 %1$s" %2$s %3$s />',
+		$cursor,
+		$half_checked,
+		$disabled
+	);
 }
 $icons_html .= '</div>';
 
 ?>
 <div class="flex items-center gap-1 whitespace-nowrap">
-	<?php if ( $show_before ) : ?>
-		<span class="text-xl font-bold"><?php echo $value; ?></span>
-	<?php endif; ?>
-	<?php echo $icons_html; ?>
-	<?php if ( null !== $total ) : ?>
-		<span class="">(<?php echo $total; ?>)</span>
-	<?php endif; ?>
+	<?php
+	if ( $show_before ) :
+		?>
+		<span class="text-xl font-bold">
+		<?php
+		echo $value;
+		?>
+		</span>
+		<?php
+	endif;
+	?>
+	<?php
+	echo $icons_html;
+	?>
+	<?php
+	if ( null !== $total ) :
+		?>
+		<span class="">(
+		<?php
+		echo $total;
+		?>
+		)</span>
+		<?php
+	endif;
+	?>
 </div>
