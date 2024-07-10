2ef984f2b9abb995dce2833486ac54e4dcd19ded
J7 <j7.dev.gg@gmail.com>
2024-07-10 21:01:40 +0800

    feat: Editor styles

--- js/src/components/course/form/CourseDescription/DescriptionDrawer/index.tsx
+++ js/src/components/course/form/CourseDescription/DescriptionDrawer/index.tsx
@@ -7,88 +7,92 @@ import { useApiUrl } from '@refinedev/core'
 
 const { Item } = Form
 
+/**
+ * TODO 取消圖片的預設 align
+ * https://github.com/Darginec05/Yoopta-Editor/issues/205
+ */
 const DescriptionDrawer = () => {
-  const apiUrl = useApiUrl()
-  const form = Form.useFormInstance()
-  const watchId = Form.useWatch(['id'], form)
-  const { yooptaEditorProps, getHtmlFromBlocks, setBlocksFromHtml } = useEditor(
-    {
-      apiEndpoint: `${apiUrl}/upload`,
-      headers: {
-        'X-WP-Nonce': window?.wpApiSettings?.nonce,
-      } as any,
-    },
-  )
+	const apiUrl = useApiUrl()
+	const form = Form.useFormInstance()
+	const watchId = Form.useWatch(['id'], form)
+	const { yooptaEditorProps, getHtmlFromBlocks, setBlocksFromHtml } = useEditor(
+		{
+			apiEndpoint: `${apiUrl}/upload`,
+			headers: {
+				'X-WP-Nonce': window?.wpApiSettings?.nonce,
+			} as any,
+		},
+	)
 
-  const { drawerProps, show, close, open } = useEditorDrawer()
+	const { drawerProps, show, close, open } = useEditorDrawer()
 
-  const handleConfirm = () => {
-    const rawHtml = getHtmlFromBlocks()
+	const handleConfirm = () => {
+		const rawHtml = getHtmlFromBlocks()
 
-    // 將 <body 與 </body 換成 <div 與 </div
-    const filteredHtml = rawHtml
-      .replace(/<body/g, '<div')
-      .replace(/<\/body>/g, '</div>')
+		// 將 <body 與 </body 換成 <div 與 </div
+		const filteredHtml = rawHtml
+			.replace(/<body/g, '<div')
+			.replace(/<\/body>/g, '</div>')
 
-    form.setFieldValue(['description'], filteredHtml)
+		form.setFieldValue(['description'], filteredHtml)
 
-    close()
-  }
+		close()
+	}
 
-  useEffect(() => {
-    if (watchId && open) {
-      const description = form.getFieldValue(['description'])
-      setBlocksFromHtml(description)
-    }
-  }, [watchId, open])
+	useEffect(() => {
+		if (watchId && open) {
+			const description = form.getFieldValue(['description'])
+			setBlocksFromHtml(description)
+		}
+	}, [watchId, open])
 
-  return (
-    <>
-      <Button type="primary" icon={<EditOutlined />} onClick={show}>
-        編輯課程重點介紹
-      </Button>
-      <Item name={['description']} label="課程重點介紹">
-        <Input.TextArea rows={8} disabled />
-      </Item>
-      <Drawer
-        {...drawerProps}
-        extra={
-          <Button type="primary" onClick={handleConfirm}>
-            確認變更
-          </Button>
-        }
-      >
-        <Alert
-          className="mb-4"
-          message="注意事項"
-          description={
-            <ol className="pl-4">
-              <li>
-                確認變更只是確認內文有沒有變更，您還是需要儲存才會存進課程
-              </li>
-              <li>可以使用 WordPress shortcode</li>
-              <li>
-                文字的上色功能、縮排效果、圖片的排列方向目前是無效的，
-                <a
-                  target="_blank"
-                  href="https://github.com/Darginec05/Yoopta-Editor/issues/205"
-                  rel="noreferrer"
-                >
-                  <GithubOutlined className="mr-1" />
-                  已通知套件作者修正
-                </a>
-              </li>
-            </ol>
-          }
-          type="warning"
-          showIcon
-          closable
-        />
+	return (
+		<>
+			<Button type="primary" icon={<EditOutlined />} onClick={show}>
+				編輯課程重點介紹
+			</Button>
+			<Item name={['description']} label="課程重點介紹" hidden>
+				<Input.TextArea rows={8} disabled />
+			</Item>
+			<Drawer
+				{...drawerProps}
+				extra={
+					<Button type="primary" onClick={handleConfirm}>
+						確認變更
+					</Button>
+				}
+			>
+				<Alert
+					className="mb-4"
+					message="注意事項"
+					description={
+						<ol className="pl-4">
+							<li>
+								確認變更只是確認內文有沒有變更，您還是需要儲存才會存進課程
+							</li>
+							<li>可以使用 WordPress shortcode</li>
+							<li>
+								文字的上色功能、縮排效果、圖片的排列方向目前是無效的，
+								<a
+									target="_blank"
+									href="https://github.com/Darginec05/Yoopta-Editor/issues/205"
+									rel="noreferrer"
+								>
+									<GithubOutlined className="mr-1" />
+									已通知套件作者修正
+								</a>
+							</li>
+						</ol>
+					}
+					type="warning"
+					showIcon
+					closable
+				/>
 
-        <Editor {...yooptaEditorProps} />
-      </Drawer>
-    </>
-  )
+				<Editor {...yooptaEditorProps} />
+			</Drawer>
+		</>
+	)
 }
 
 export default DescriptionDrawer
