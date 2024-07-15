4a5e9d331455dfd3be8fa06d17b70ace13f9f9ba
J7 <j7.dev.gg@gmail.com>
2024-07-15 14:11:59 +0800

    feat: remove unused

--- js/src/main.tsx
+++ js/src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { app1Selector, app2Selector } from '@/utils'
+import { app1Selector } from '@/utils'
 import { StyleProvider } from '@ant-design/cssinjs'
 
 const App1 = React.lazy(() => import('./App1'))
-const App2 = React.lazy(() => import('./App2'))
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,16 +16,11 @@ const queryClient = new QueryClient({
 })
 
 const app1Nodes = document.querySelectorAll(app1Selector)
-const app2Nodes = document.querySelectorAll(app2Selector)
 
 const mapping = [
   {
     els: app1Nodes,
     App: App1,
-  },
-  {
-    els: app2Nodes,
-    App: App2,
   },
 ]
 
