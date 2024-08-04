8cbfd5e6aba0102bfda394b3a0a4912ac1fe2fb7
J7 <j7.dev.gg@outlook.com>
2024-08-04 14:38:00 +0800

    fix: 教學圖片馬賽克

--- js/src/pages/admin/Settings/index.tsx
+++ js/src/pages/admin/Settings/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Form, Input, Button } from 'antd'
+import { Form, Input, Button, Alert } from 'antd'
 import useOptions from './hooks/useOptions'
 import useSave from './hooks/useSave'
 import { FiSwitch } from '@/components/formItem'
 import { SimpleImage } from '@/components/general'
-import bunnyTutorial1 from '@/assets/images/bunny-tutorial-1.png'
-import bunnyTutorial2 from '@/assets/images/bunny-tutorial-2.png'
+import bunnyTutorial1 from '@/assets/images/bunny-tutorial-1.jpg'
+import bunnyTutorial2 from '@/assets/images/bunny-tutorial-2.jpg'
+import { RxExternalLink } from 'react-icons/rx'
 
 const { Item } = Form
 
@@ -54,6 +55,24 @@ const index = () => {
 				</div>
 				<div className="flex-1 h-auto md:h-screen md:overflow-y-auto">
 					<p className="font-bold mb-4">說明</p>
+					<Alert
+						message="沒有 Bunny 帳號？"
+						description={
+							<>
+								若還沒有 Bunny 帳號，可以
+								<a
+									href="https://bunny.net?ref=wd7c7lcrv4"
+									target="_blank"
+									rel="noopener noreferrer"
+									className="ml-2 font-bold"
+								>
+									點此申請 <RxExternalLink className="relative top-0.5" />
+								</a>
+							</>
+						}
+						type="info"
+						showIcon
+					/>
 					<div className="mb-4">
 						<p>1. 前往 Bunny 後台，選擇 「Stream」 並進入 「Library」</p>
 						<SimpleImage src={bunnyTutorial1} className="w-full aspect-[2.1]" />
