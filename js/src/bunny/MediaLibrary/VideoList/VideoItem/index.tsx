6ffaab88076a7fbfef0f8406c425ee192768a894
J7 <j7.dev.gg@gmail.com>
2024-08-13 12:21:10 +0800

    feat: 選擇實有勾勾提示

--- js/src/bunny/MediaLibrary/VideoList/VideoItem/index.tsx
+++ js/src/bunny/MediaLibrary/VideoList/VideoItem/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { FC, useState, HTMLAttributes } from 'react'
 import { TVideo } from '@/bunny/MediaLibrary/types'
 import { bunny_cdn_hostname } from '@/utils'
 import { SimpleImage } from '@/components/general'
@@ -6,6 +6,28 @@ import { Typography, message } from 'antd'
 
 const PREVIEW_FILENAME = 'preview.webp'
 const { Text } = Typography
+
+const CheckIcon: FC<HTMLAttributes<SVGElement>> = (props) => {
+	return (
+		<svg
+			viewBox="0 0 20 20"
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			{...props}
+		>
+			<g strokeWidth="0"></g>
+			<g strokeLinecap="round" strokeLinejoin="round"></g>
+			<g>
+				{' '}
+				<path
+					fill="#000000"
+					fillRule="evenodd"
+					d="M3 10a7 7 0 019.307-6.611 1 1 0 00.658-1.889 9 9 0 105.98 7.501 1 1 0 00-1.988.22A7 7 0 113 10zm14.75-5.338a1 1 0 00-1.5-1.324l-6.435 7.28-3.183-2.593a1 1 0 00-1.264 1.55l3.929 3.2a1 1 0 001.38-.113l7.072-8z"
+				></path>{' '}
+			</g>
+		</svg>
+	)
+}
 
 const VideoItem = ({
 	children,
@@ -46,7 +68,7 @@ const VideoItem = ({
 	}
 
 	return (
-		<div className="w-36">
+		<div className="w-36 relative">
 			<SimpleImage
 				onClick={handleClick}
 				onMouseEnter={() => {
@@ -68,6 +90,11 @@ const VideoItem = ({
 			<Text className=" text-xs" ellipsis>
 				{video.title}
 			</Text>
+			{isSelected && (
+				<div className="bg-white absolute -top-2 -right-2 z-50 w-6 h-6 -1 rounded-full flex items-center justify-center">
+					<CheckIcon className="w-5 h-5 [&_path]:fill-yellow-300" />
+				</div>
+			)}
 		</div>
 	)
 }
