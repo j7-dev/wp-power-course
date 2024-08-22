79fbc7667dde8724ba30b73e69cfe2ad13e293aa
J7 <j7.dev.gg@gmail.com>
2024-08-22 17:16:56 +0800

    fix: 允許 chapter 編輯器存入 svg

--- inc/class/api/class-chapter.php
+++ inc/class/api/class-chapter.php
@@ -88,15 +88,11 @@ final class Chapter {
 
 		$body_params = ChapterFactory::converter( $body_params );
 
-		// sanitize_text_field 會過濾 html tag ，description 需要保留 html tag，使用 wp_kses_post
-		$sanitize_description = \wp_kses_post( $body_params['post_content'] ?? '' );
-
 		$skip_keys   = [
 			'chapter_video',
+			'post_content',
 		];
 		$body_params = WP::sanitize_text_field_deep($body_params, true, $skip_keys);
-
-		$body_params['post_content'] = $sanitize_description;
 
 		// 將 '[]' 轉為 []
 		$body_params = Base::format_empty_array( $body_params );
