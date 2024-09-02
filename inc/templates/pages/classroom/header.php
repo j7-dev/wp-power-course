10fbcc97508e1b9e6cf57a3c1705705a61dd641e
J7 <j7.dev.gg@gmail.com>
2024-09-02 19:13:22 +0800

    fix: classroom action 會被留言頭像蓋住 css 調整

--- inc/templates/pages/classroom/header.php
+++ inc/templates/pages/classroom/header.php
@@ -102,7 +102,7 @@ if (count($chapter_ids) > 0) {
 // render
 printf(
 	/*html*/'
-<div id="pc-classroom-header" class="w-full bg-white py-4 px-4 lg:px-6 flex flex-col lg:flex-row justify-between lg:items-center top-0 z-20" style="position:fixed;">
+<div id="pc-classroom-header" class="w-full bg-white py-4 px-4 lg:px-6 flex flex-col lg:flex-row justify-between lg:items-center top-0 z-30" style="position:fixed;">
   <div class="flex flex-nowrap gap-4 items-end">
 		<h2 id="classroom-chapter_title" class="text-sm lg:text-base text-bold lg:tracking-wide my-0 line-clamp-1 pl-6 lg:pl-0">%1$s</h2>
 		%2$s
