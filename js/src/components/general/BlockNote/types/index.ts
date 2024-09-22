a9ca1b875e60d4ce07c0de48399ba6266f09e314
J7 <j7.dev.gg@outlook.com>
2024-09-23 00:15:11 +0800

    chore

--- js/src/components/general/BlockNote/types/index.ts
+++ js/src/components/general/BlockNote/types/index.ts
@@ -1,26 +1,26 @@
 import {
-  DefaultStyleSchema,
-  DefaultInlineContentSchema,
-  BlockNoteEditorOptions,
+	DefaultStyleSchema,
+	DefaultInlineContentSchema,
+	BlockNoteEditorOptions,
 } from '@blocknote/core'
 import { DefaultReactSuggestionItem } from '@blocknote/react'
 import { schema } from '../useBlockNote'
 
 export type TApiConfig = {
-  apiEndpoint: string
-  headers?: Headers
+	apiEndpoint: string
+	headers?: Headers
 }
 
 export type TUseBlockNoteParams = {
-  options?: BlockNoteEditorOptions<
-    typeof schema.blockSchema,
-    DefaultInlineContentSchema,
-    DefaultStyleSchema
-  >
-  deps?: React.DependencyList
-  apiConfig: TApiConfig
-  itemsFilter?: (
-    _items: DefaultReactSuggestionItem[],
-    _query: string,
-  ) => DefaultReactSuggestionItem[]
+	options?: BlockNoteEditorOptions<
+		typeof schema.blockSchema,
+		DefaultInlineContentSchema,
+		DefaultStyleSchema
+	>
+	deps?: React.DependencyList
+	apiConfig: TApiConfig
+	itemsFilter?: (
+		_items: DefaultReactSuggestionItem[],
+		_query: string,
+	) => DefaultReactSuggestionItem[]
 }
