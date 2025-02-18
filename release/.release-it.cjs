be3aa43fbd1a7601d4fc05ae4c36401d9d993bd0
J7 <j7.dev.gg@outlook.com>
2025-02-18 09:19:57 +0800

    feat: 修改打包指令

--- release/.release-it.cjs
+++ release/.release-it.cjs
@@ -28,10 +28,12 @@ module.exports = {
 		push: release,
 	},
 	hooks: {
-		// 'before:init': [], // run before initialization
+		'before:init': [
+			'yarn build && yarn build:wp && echo ✅ build success',
+		], // run before initialization
 		// 'after:[my-plugin]:bump': './bin/my-script.sh', // run after bumping version of my-plugin
 		'after:bump': [
-			'yarn build && echo ✅ build success',
+
 			release
 				? 'yarn sync:version && echo ✅ sync version success'
 				: 'echo 🚫 skip sync version',
--- vite.config-for-wp.ts
+++ vite.config-for-wp.ts
@@ -7,41 +7,41 @@ import optimizer from 'vite-plugin-optimizer'
 import { terser } from 'rollup-plugin-terser'
 
 export default defineConfig({
-  build: {
-    emptyOutDir: true,
-    minify: true,
-    outDir: path.resolve(__dirname, 'inc/assets/dist'),
-    watch: {
-      include: 'inc/**',
-      exclude:
-        'js/**, modules/**, node_modules/**, release/**, vendor/**, .git/**, .vscode/**',
-    },
-    rollupOptions: {
-      input: 'inc/assets/src/main.ts', // Optional, defaults to 'src/main.js'.
-      output: {
-        assetFileNames: '[ext]/index.[ext]',
-        entryFileNames: 'index.js',
-      },
-    },
-  },
-  plugins: [
-    alias(),
-    tsconfigPaths(),
-    liveReload([
-      __dirname + '/**/*.php',
-    ]),
-    optimizer({
-      jquery: 'const $ = window.jQuery; export { $ as default }',
-    }),
-    terser({
-      mangle: {
-        reserved: ['$'], // 指定 $ 不被改變
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'inc/assets/src'),
-    },
-  },
+	build: {
+		emptyOutDir: true,
+		minify: true,
+		outDir: path.resolve(__dirname, 'inc/assets/dist'),
+		// watch: {
+		//   include: 'inc/**',
+		//   exclude:
+		//     'js/**, modules/**, node_modules/**, release/**, vendor/**, .git/**, .vscode/**',
+		// },
+		rollupOptions: {
+			input: 'inc/assets/src/main.ts', // Optional, defaults to 'src/main.js'.
+			output: {
+				assetFileNames: '[ext]/index.[ext]',
+				entryFileNames: 'index.js',
+			},
+		},
+	},
+	plugins: [
+		alias(),
+		tsconfigPaths(),
+		liveReload([
+			__dirname + '/**/*.php',
+		]),
+		optimizer({
+			jquery: 'const $ = window.jQuery; export { $ as default }',
+		}),
+		terser({
+			mangle: {
+				reserved: ['$'], // 指定 $ 不被改變
+			},
+		}),
+	],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'inc/assets/src'),
+		},
+	},
 })
