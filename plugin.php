789360103c956a35ce38a272652fd767f23335dc
J7 <j7.dev.gg@gmail.com>
2025-02-21 18:39:09 +0800

    feat: deprecated ApiOptimize

--- plugin.php
+++ plugin.php
@@ -55,7 +55,7 @@ if (!\class_exists('J7\PowerCourse\Plugin')) {
 					'name'     => 'Powerhouse',
 					'slug'     => 'powerhouse',
 					'source'   => 'https://github.com/j7-dev/wp-powerhouse/releases/latest/download/powerhouse.zip',
-					'version'  => '3.0.3',
+					'version'  => '3.0.4',
 					'required' => true,
 				],
 			];
