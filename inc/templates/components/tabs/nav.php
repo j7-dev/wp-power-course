a1272005c3acd5f7f620c4295ae7cf301540df28
J7 <j7.dev.gg@gmail.com>
2025-02-26 12:10:46 +0800

    feat: 調整銷售業 TAB 配色

--- inc/templates/components/tabs/nav.php
+++ inc/templates/components/tabs/nav.php
@@ -17,11 +17,11 @@ $course_tabs        = is_array($course_tabs) ? $course_tabs : []; // @phpstan-ig
 $default_active_key = $args['default_active_key'] ?? array_key_first($course_tabs);
 
 
-echo '<div class="bg-base-100 flex gap-0 text-base-content justify-between [&_.active]:!border-gray-600 [&_.active]:text-gray-600 [&_div:hover]:!border-gray-600 [&_div:hover]:text-gray-600 border-base-content/20" style="border-bottom: 3px solid;">';
+echo '<div class="bg-base-100 flex gap-0 justify-between [&_.active]:!border-primary [&_.active]:text-primary [&_div:hover]:!border-primary [&_div:hover]:text-primary text-base-content/30" style="border-bottom: 2px solid var(--fallback-bc,oklch(var(--bc)/0.3));">';
 
 foreach ( $course_tabs as $key => $course_tab ) {
 	printf(
-		/*html*/'<div id="tab-nav-%1$s" class="cursor-pointer w-full text-center py-2 px-2 lg:px-8 relative -bottom-[3px] font-normal lg:font-semibold transition text-sm lg:text-base duration-300 ease-in-out %2$s" style="border-bottom: 3px solid transparent;">%3$s</div>',
+		/*html*/'<div id="tab-nav-%1$s" class="cursor-pointer w-full text-center py-2 px-2 lg:px-8 relative -bottom-[3px] font-normal lg:font-semibold transition text-sm lg:text-base duration-300 ease-in-out %2$s" style="border-bottom: 2px solid;">%3$s</div>',
 		$key,
 		$default_active_key === $key ? 'active' : '',
 		$course_tab['label']
