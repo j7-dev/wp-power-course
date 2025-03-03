fdff250c4c870e1533bf30a57f8605c44d63c5b0
J7 <j7.dev.gg@gmail.com>
2025-03-03 18:40:40 +0800

    refactor: sider 手機板

--- inc/templates/pages/classroom/sider.php
+++ inc/templates/pages/classroom/sider.php
@@ -25,26 +25,44 @@ if ( ! ( $product instanceof \WC_Product ) ) {
 	throw new \Exception( 'product 不是 WC_Product' );
 }
 
-
 printf(
 	/*html*/'
-<div id="pc-classroom-sider" class="tw-hidden lg:tw-block w-[25rem] bg-base-100 z-20 left-0 top-0 h-screen expended"
-	style="border-right: 1px solid var(--fallback-bc,oklch(var(--bc)/.1));position:fixed;left:0px">
-	<div id="pc-classroom-sider__main" class="h-full flex flex-col [&_.pc-sider-chapters]:flex-1 [&_.pc-sider-chapters]:pb-12">
-		%1$s
-		<div class="py-4 pl-9 absolute bottom-0 w-full bg-base-100">
-			<a
-				href="%2$s"
-				class="hover:opacity-75 transition duration-300 flex gap-4 items-center"
-			>
-				<img class="size-6" src="%3$s" />
-				<span class="text-gray-400 font-light">
-						回《我的學習》
-				</span>
-			</a>
+	<div class="pc-drawer lg:pc-drawer-open h-0">
+		<input id="pc-classroom-drawer" type="checkbox" class="pc-drawer-toggle" />
+		<div class="pc-drawer-content">
+			<div class="h-10 w-full bg-base-200 flex lg:tw-hidden items-center justify-between px-4 tw-fixed top-[52px] left-0 z-30" style="border-bottom: 1px solid var(--fallback-bc,oklch(var(--bc)/.1))">
+				<label for="pc-classroom-drawer" class="flex items-center gap-x-2 text-sm cursor-pointer">
+					<svg class="size-4 stroke-base-content/30" viewBox="0 0 48 48" fill="none">
+						<path d="M42 9H6" stroke="#78716c" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"></path>
+						<path d="M34 19H6" stroke="#78716c" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"></path>
+						<path d="M42 29H6" stroke="#78716c" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"></path>
+						<path d="M34 39H6" stroke="#78716c" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"></path>
+					</svg>
+					選單
+				</label>
+			</div>
+		</div>
+		<div class="pc-drawer-side z-40">
+			<div id="pc-sider" class="w-4/5 px-2 max-w-[25rem] lg:w-[25rem] h-screen bg-base-100 z-40"
+				style="border-right: 1px solid var(--fallback-bc,oklch(var(--bc)/.1));position:fixed;left:0px">
+				<div id="pc-sider__main" class="h-full flex flex-col [&_.pc-sider-chapters]:flex-1 [&_.pc-sider-chapters]:pb-12">
+					%1$s
+					<div class="py-4 pl-9 absolute bottom-0 left-0 w-full bg-base-100">
+						<a
+							href="%2$s"
+							class="hover:opacity-75 transition duration-300 flex gap-4 items-center"
+						>
+							<img class="size-6" src="%3$s" />
+							<span class="text-gray-400 font-light">
+									回《我的學習》
+							</span>
+						</a>
+					</div>
+				</div>
+			</div>
+			<label for="pc-classroom-drawer" aria-label="close sidebar" class="pc-drawer-overlay w-full h-full"></label>
 		</div>
 	</div>
-</div>
 ',
 	Plugin::load_template(
 		'classroom/chapters',
