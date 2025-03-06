9548fc2a81fc8207e33cf04bfe5687de8c156085
J7 <j7.dev.gg@gmail.com>
2025-03-06 20:10:02 +0800

    feat: 兼容 儲存章節編輯器

--- inc/classes/Compatibility/Chapter.php
+++ inc/classes/Compatibility/Chapter.php
@@ -95,4 +95,57 @@ final class Chapter {
 			\J7\WpUtils\Classes\WC::log($e->getMessage(), 'Chapter::migrate_chapter_to_new_structure 出錯了');
 		}
 	}
+
+
+	/**
+	 * 將 chapter 設定 editor
+	 * 將使用 elementor 的 chapter 設定 editor 為 elementor
+	 * 將未使用 elementor 的 chapter 設定 editor 為 power-editor
+	 */
+	public static function set_editor_meta_to_chapter(): void {
+
+		$elementor_chapter_ids = \get_posts(
+			[
+				'post_type'   => CPT::POST_TYPE,
+				'numberposts' => -1,
+				'meta_query'  => [
+					'relation'              => 'AND',
+					'elementor_data_clause' => [
+						'key'     => '_elementor_data',
+						'compare' => 'EXISTS',
+					],
+					'editor_clause'         => [
+						'key'     => 'editor',
+						'compare' => 'NOT EXISTS',
+					],
+				],
+			]
+		);
+
+		foreach ($elementor_chapter_ids as $chapter_id) {
+			\update_post_meta($chapter_id, 'editor', 'elementor');
+		}
+
+		$power_chapter_ids = \get_posts(
+			[
+				'post_type'   => CPT::POST_TYPE,
+				'numberposts' => -1,
+				'meta_query'  => [
+					'relation'              => 'AND',
+					'elementor_data_clause' => [
+						'key'     => '_elementor_data',
+						'compare' => 'NOT EXISTS',
+					],
+					'editor_clause'         => [
+						'key'     => 'editor',
+						'compare' => 'NOT EXISTS',
+					],
+				],
+			]
+			);
+
+		foreach ($power_chapter_ids as $chapter_id) {
+			\update_post_meta($chapter_id, 'editor', 'power-editor');
+		}
+	}
 }
