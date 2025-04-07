999cef2e6563b84e9b88d4635a85d067edcf9a0d
J7 <j7.dev.gg@gmail.com>
2025-04-07 12:43:10 +0800

    chore: add deprecate

--- js/src/components/user/UserTable/Card.tsx
+++ js/src/components/user/UserTable/Card.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Card as AntdCard, CardProps } from 'antd'
 
+/**
+ * @deprecated 從 antd-toolkit 1.3.107 版本開始已提供 Card 組件，建議改用該組件
+ */
 const Card = ({
 	children,
 	showCard = true,
