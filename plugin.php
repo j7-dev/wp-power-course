4fc680a58e859c19fab34572ad5bcc929a84dbba
J7 <j7.dev.gg@gmail.com>
2025-04-15 17:50:28 +0800

    feat: 修改 class & 依賴高版本 powerhouse

--- plugin.php
+++ plugin.php
@@ -60,7 +60,7 @@ if (!\class_exists('J7\PowerCourse\Plugin')) {
 					'name'     => 'Powerhouse',
 					'slug'     => 'powerhouse',
 					'source'   => 'https://github.com/j7-dev/wp-powerhouse/releases/latest/download/powerhouse.zip',
-					'version'  => '3.2.4',
+					'version'  => '3.2.11',
 					'required' => true,
 				],
 			];
