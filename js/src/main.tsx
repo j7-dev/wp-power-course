8961fec62d4c511896d186159d3bb016ce0a7a8f
J7 <j7.dev.gg@gmail.com>
2025-04-28 12:39:56 +0800

    fix: render React after DOMContentLoaded

--- js/src/main.tsx
+++ js/src/main.tsx
@@ -5,9 +5,9 @@ import { app1Selector, app2Selector } from '@/utils'
 import { StyleProvider } from '@ant-design/cssinjs'
 import { TPlayerProps } from './App2/Player'
 import { PageLoading } from '@/components/general'
+import App1 from './App1'
+import App2 from './App2'
 
-const App1 = lazy(() => import('./App1'))
-const App2 = lazy(() => import('./App2'))
 const queryClient = new QueryClient({
 	defaultOptions: {
 		queries: {
@@ -26,32 +26,34 @@ const mapping = [
 	},
 ]
 
-mapping.forEach(({ els, App }) => {
-	if (!!els) {
-		els.forEach((el) => {
-			ReactDOM.createRoot(el).render(
-				<QueryClientProvider client={queryClient}>
-					<StyleProvider hashPriority="low">
-						<Suspense fallback={<PageLoading type="general" />}>
-							<App />
-						</Suspense>
-					</StyleProvider>
-				</QueryClientProvider>,
-			)
-		})
-	}
-})
+document.addEventListener('DOMContentLoaded', () => {
+	mapping.forEach(({ els, App }) => {
+		if (!!els) {
+			els.forEach((el) => {
+				ReactDOM.createRoot(el).render(
+					<QueryClientProvider client={queryClient}>
+						<StyleProvider hashPriority="low">
+							<Suspense fallback={<PageLoading type="general" />}>
+								<App />
+							</Suspense>
+						</StyleProvider>
+					</QueryClientProvider>,
+				)
+			})
+		}
+	})
 
-// 一個畫面可能會有多個 vidstack 元素
-const vidstackNodes = document.querySelectorAll(app2Selector)
-vidstackNodes.forEach((vidstackNode) => {
-	const dataset: TPlayerProps = vidstackNode.dataset || {}
+	// 一個畫面可能會有多個 vidstack 元素
+	const vidstackNodes = document.querySelectorAll(app2Selector)
+	vidstackNodes.forEach((vidstackNode) => {
+		const dataset: TPlayerProps = vidstackNode.dataset || {}
 
-	ReactDOM.createRoot(vidstackNode).render(
-		<React.StrictMode>
-			<Suspense fallback={<PageLoading type="general" />}>
-				<App2 {...dataset} />
-			</Suspense>
-		</React.StrictMode>,
-	)
+		ReactDOM.createRoot(vidstackNode).render(
+			<React.StrictMode>
+				<Suspense fallback={<PageLoading type="general" />}>
+					<App2 {...dataset} />
+				</Suspense>
+			</React.StrictMode>,
+		)
+	})
 })
