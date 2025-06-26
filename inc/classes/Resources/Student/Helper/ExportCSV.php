73c20dbcb6f2ef917389970a583eadc043788ce1
J7 <j7.dev.gg@gmail.com>
2025-06-26 17:10:21 +0800

    feat: 新增匯出欄位

--- inc/classes/Resources/Student/Helper/ExportCSV.php
+++ inc/classes/Resources/Student/Helper/ExportCSV.php
@@ -5,6 +5,9 @@ declare (strict_types = 1);
 namespace J7\PowerCourse\Resources\Student\Helper;
 
 use J7\Powerhouse\Utils\ExportCSV as ExportCSVBase;
+use J7\PowerCourse\Utils\Course as CourseUtils;
+use J7\PowerCourse\Resources\Course\ExpireDate;
+use J7\Powerhouse\Utils\Base as PowerhouseUtils;
 
 /**
  * ExportCSV 匯出 CSV
@@ -35,12 +38,16 @@ final class ExportCSV extends ExportCSVBase {
 		$this->rows = $this->get_rows();
 
 		$this->columns = [
-			'user_id'      => '學員 ID',
-			'display_name' => '學員名稱',
-			'user_email'   => '學員 Email',
-			'course_name'  => '課程名稱',
-			'course_id'    => '課程 ID',
-			'progress'     => '學習進度',
+			'user_id'           => '學員 ID',
+			'display_name'      => '學員名稱',
+			'user_email'        => '學員 Email',
+			'user_registered'   => '學員註冊時間',
+			'course_name'       => '課程名稱',
+			'course_id'         => '課程 ID',
+			'progress'          => '學習進度',
+			'expire_date_label' => '觀看期限',
+			'is_expired'        => '是否過期',
+			'subscription_id'   => '訂閱 ID',
 		];
 	}
 
@@ -50,7 +57,9 @@ final class ExportCSV extends ExportCSVBase {
 	 * @return array{user_id: int, display_name: string, user_email: string, course_name: string, course_id: int, progress: string}
 	 * */
 	private function get_rows(): array {
-		$query = new Query(
+		try {
+
+			$query = new Query(
 			[
 				'posts_per_page' => -1,
 				'meta_key'       => 'avl_course_ids',
@@ -58,20 +67,37 @@ final class ExportCSV extends ExportCSVBase {
 			]
 			);
 
-		$users = $query->get_users();
+			$users = $query->get_users();
 
-		$rows = [];
+			$rows = [];
 
-		foreach ($users as $user) {
-			$rows[] = (object) [
-				'user_id'      => $user->ID,
-				'display_name' => $user->display_name,
-				'user_email'   => $user->user_email,
-				'course_name'  => $this->course_name,
-				'course_id'    => $this->course_id,
-				'progress'     => '',
-			];
+			PowerhouseUtils::batch_process(
+			$users,
+			function ( $user ) use ( &$rows ) {
+				$expire_date = ExpireDate::instance($this->course_id, $user->ID);
+
+				$rows[] = (object) [
+					'user_id'           => $user->ID,
+					'display_name'      => $user->display_name,
+					'user_email'        => $user->user_email,
+					'user_registered'   => $user->user_registered,
+					'course_name'       => $this->course_name,
+					'course_id'         => $this->course_id,
+					'progress'          => CourseUtils::get_course_progress( $this->course_id, $user->ID ) . '%',
+					'expire_date_label' => $expire_date->expire_date_label,
+					'is_expired'        => $expire_date->is_expired ? '是' : '否',
+					'subscription_id'   => $expire_date->subscription_id ?? '',
+				];
+			}
+			);
+
+			return $rows;
+		} catch (\Throwable $th) {
+			\J7\WpUtils\Classes\WC::logger(
+				"課程 #{$this->course_id} 學員 CSV 匯出失敗，{$th->getMessage()}",
+				'error'
+			);
+			return [];
 		}
-		return $rows;
 	}
 }
