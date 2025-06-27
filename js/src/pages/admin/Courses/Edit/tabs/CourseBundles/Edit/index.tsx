ed1680ca01cf64a738f7c973a90dd42519423433
J7 <j7.dev.gg@gmail.com>
2025-06-27 17:46:42 +0800

    refactor: formatDateRangeData 改由 antd-toolkit import

--- js/src/pages/admin/Courses/Edit/tabs/CourseBundles/Edit/index.tsx
+++ js/src/pages/admin/Courses/Edit/tabs/CourseBundles/Edit/index.tsx
@@ -3,14 +3,13 @@ import { Form, Switch, Alert, message } from 'antd'
 import { TBundleProductRecord } from '@/components/product/ProductTable/types'
 import { TCourseRecord } from '@/pages/admin/Courses/List/types'
 import { Edit, useForm } from '@refinedev/antd'
-import { formatDateRangeData } from '@/utils'
-import { toFormData } from 'antd-toolkit'
 import { ExclamationCircleFilled } from '@ant-design/icons'
 import BundleForm from './BundleForm'
 import { Dayjs } from 'dayjs'
 import { useAtom, useSetAtom, useAtomValue } from 'jotai'
 import { selectedProductsAtom, courseAtom, bundleProductAtom } from './atom'
 import { useLink } from '@refinedev/core'
+import { toFormData, formatDateRangeData } from 'antd-toolkit'
 
 const EditBundleComponent = ({
 	record,
