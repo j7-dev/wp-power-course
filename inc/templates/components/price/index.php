d3e62eb509c0e62366796d44ac20b7313e0eb20d
J7 <j7.dev.gg@gmail.com>
2025-07-10 17:18:20 +0800

    chore: 移除不用代碼

--- inc/templates/components/price/index.php
+++ inc/templates/components/price/index.php
@@ -29,34 +29,3 @@ $price_html = Base::get_price_html( $product );
 echo '<div class="pc-price-html">';
 echo $price_html;
 echo '</div>';
-return;
-
-
-$is_on_sale    = $product->is_on_sale();
-$regular_price = $product->get_regular_price();
-$sale_price    = $product->get_sale_price();
-
-
-if ($is_on_sale) {
-	printf(
-		/*html*/'
-		<div class="flex flex-col">
-			<del aria-hidden="true" class="text-gray-600">%1$s</del>
-			<ins class="text-error text-2xl font-semibold">%2$s</ins>
-		</div>
-		',
-		( is_numeric( $regular_price ) ? \wc_price( (float) $regular_price ) : $regular_price ),
-		( is_numeric( $sale_price ) ? \wc_price( (float) $sale_price ) : $sale_price )
-		);
-	return;
-}
-
-
-printf(
-		/*html*/'
-		<div class="flex flex-col">
-			<ins class="text-gray-600 text-2xl font-semibold">%1$s</ins>
-		</div>
-		',
-		( is_numeric( $regular_price ) ? \wc_price( (float) $regular_price ) : $regular_price ),
-		);
