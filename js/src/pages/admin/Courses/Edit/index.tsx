24a1b1328a9ea2878bee28877a09b5011523bc16
J7 <j7.dev.gg@gmail.com>
2025-09-08 13:27:57 +0800

    feat: 新增課程 edit_url, 可以直接前往傳統介面編輯

--- js/src/pages/admin/Courses/Edit/index.tsx
+++ js/src/pages/admin/Courses/Edit/index.tsx
@@ -1,215 +1,224 @@
-import { memo, useMemo, useState } from 'react'
-import { Edit, useForm } from '@refinedev/antd'
-import { Tabs, TabsProps, Form, Switch, Button, Tooltip, FormProps } from 'antd'
-import { SortableChapters } from '@/components/course'
-import { TCourseRecord } from '@/pages/admin/Courses/List/types'
-import { RecordContext } from '@/pages/admin/Courses/Edit/hooks'
+import {memo, useMemo, useState} from 'react'
+import {Edit, useForm} from '@refinedev/antd'
+import {Tabs, TabsProps, Form, Switch, Button, Tooltip, FormProps} from 'antd'
+import {SortableChapters} from '@/components/course'
+import {TCourseRecord} from '@/pages/admin/Courses/List/types'
+import {RecordContext} from '@/pages/admin/Courses/Edit/hooks'
 import {
-	CourseDescription,
-	CourseQA,
-	CourseAnnouncement,
-	CoursePrice,
-	CourseBundles,
-	CourseOther,
-	CourseStudents,
-	CourseAnalysis,
+    CourseDescription,
+    CourseQA,
+    // CourseAnnouncement,
+    CoursePrice,
+    CourseBundles,
+    CourseOther,
+    CourseStudents,
+    CourseAnalysis,
 } from '@/pages/admin/Courses/Edit/tabs'
-import { useEnv } from '@/hooks'
-import { toFormData, formatDateRangeData } from 'antd-toolkit'
+import {useEnv} from '@/hooks'
+import {toFormData, formatDateRangeData} from 'antd-toolkit'
 
-const { Item } = Form
+const {Item} = Form
 
 export const CoursesEdit = () => {
-	const { SITE_URL, COURSE_PERMALINK_STRUCTURE } = useEnv()
-	const [activeKey, setActiveKey] = useState('CourseDescription')
-	// 初始化資料
-	const {
-		formProps: _formProps,
-		saveButtonProps,
-		query,
-		mutation,
-		onFinish,
-	} = useForm<TCourseRecord>({
-		dataProviderName: 'power-course',
-		redirect: false,
-	})
+    const {SITE_URL, COURSE_PERMALINK_STRUCTURE} = useEnv()
+    const [activeKey, setActiveKey] = useState('CourseDescription')
+    // 初始化資料
+    const {
+        formProps: _formProps,
+        saveButtonProps,
+        query,
+        mutation,
+        onFinish,
+    } = useForm<TCourseRecord>({
+        dataProviderName: 'power-course',
+        redirect: false,
+    })
 
-	const record = useMemo(() => {
-		return query?.data?.data
-	}, [query])
+    const record = useMemo(() => {
+        return query?.data?.data
+    }, [query])
 
-	// 將 [] 轉為 '[]'，例如，清除原本分類時，如果空的，前端會是 undefined，轉成 formData 時會遺失
-	const handleOnFinish = (values: Partial<TCourseRecord>) => {
-		const formattedValues = formatDateRangeData(values, 'sale_date_range', [
-			'date_on_sale_from',
-			'date_on_sale_to',
-		])
-		const { description, short_description, ...rest } = formattedValues
-		onFinish(toFormData(rest))
-	}
+    // 將 [] 轉為 '[]'，例如，清除原本分類時，如果空的，前端會是 undefined，轉成 formData 時會遺失
+    const handleOnFinish = (values: Partial<TCourseRecord>) => {
+        const formattedValues = formatDateRangeData(values, 'sale_date_range', [
+            'date_on_sale_from',
+            'date_on_sale_to',
+        ])
+        const {description, short_description, ...rest} = formattedValues
+        onFinish(toFormData(rest))
+    }
 
-	// 重組 formProps
-	const formProps: FormProps = {
-		..._formProps,
-		layout: 'vertical',
-		onFinish: handleOnFinish,
-	}
+    // 重組 formProps
+    const formProps: FormProps = {
+        ..._formProps,
+        layout: 'vertical',
+        onFinish: handleOnFinish,
+    }
 
-	// TAB items
-	const items: TabsProps['items'] = [
-		{
-			key: 'CourseDescription',
-			forceRender: true,
-			label: '課程描述',
-			children: <CourseDescription formProps={formProps} />,
-		},
-		{
-			key: 'CoursePrice',
-			forceRender: true,
-			label: '課程訂價',
-			children: <CoursePrice formProps={formProps} />,
-		},
-		{
-			key: 'CourseBundle',
-			forceRender: false,
-			label: '銷售方案',
-			children: <CourseBundles />,
-		},
-		{
-			key: 'Chapters',
-			forceRender: false,
-			label: '章節管理',
-			children: <SortableChapters />,
-		},
-		{
-			key: 'CourseQA',
-			forceRender: true,
-			label: 'QA設定',
-			children: <CourseQA formProps={formProps} />,
-		},
-		// {
-		// 	key: 'CourseAnnouncement',
-		// 	forceRender: false,
-		// 	label: '課程公告',
-		// 	children: <CourseAnnouncement />,
-		// },
-		{
-			key: 'CourseOther',
-			forceRender: true,
-			label: '其他設定',
-			children: <CourseOther formProps={formProps} />,
-		},
-		{
-			key: 'CourseStudents',
-			forceRender: false,
-			label: '學員管理',
-			children: <CourseStudents />,
-		},
-		{
-			key: 'CourseAnalysis',
-			forceRender: false,
-			label: '分析',
-			children: <CourseAnalysis />,
-		},
-	]
+    // TAB items
+    const items: TabsProps['items'] = [
+        {
+            key: 'CourseDescription',
+            forceRender: true,
+            label: '課程描述',
+            children: <CourseDescription formProps={formProps}/>,
+        },
+        {
+            key: 'CoursePrice',
+            forceRender: true,
+            label: '課程訂價',
+            children: <CoursePrice formProps={formProps}/>,
+        },
+        {
+            key: 'CourseBundle',
+            forceRender: false,
+            label: '銷售方案',
+            children: <CourseBundles/>,
+        },
+        {
+            key: 'Chapters',
+            forceRender: false,
+            label: '章節管理',
+            children: <SortableChapters/>,
+        },
+        {
+            key: 'CourseQA',
+            forceRender: true,
+            label: 'QA設定',
+            children: <CourseQA formProps={formProps}/>,
+        },
+        // {
+        // 	key: 'CourseAnnouncement',
+        // 	forceRender: false,
+        // 	label: '課程公告',
+        // 	children: <CourseAnnouncement />,
+        // },
+        {
+            key: 'CourseOther',
+            forceRender: true,
+            label: '其他設定',
+            children: <CourseOther formProps={formProps}/>,
+        },
+        {
+            key: 'CourseStudents',
+            forceRender: false,
+            label: '學員管理',
+            children: <CourseStudents/>,
+        },
+        {
+            key: 'CourseAnalysis',
+            forceRender: false,
+            label: '分析',
+            children: <CourseAnalysis/>,
+        },
+    ]
 
-	const disableSaveButton = [
-		'CourseBundle',
-		'Chapters',
-		'CourseStudents',
-		'CourseAnalysis',
-	].includes(activeKey)
+    const disableSaveButton = [
+        'CourseBundle',
+        'Chapters',
+        'CourseStudents',
+        'CourseAnalysis',
+    ].includes(activeKey)
 
-	return (
-		<div className="sticky-card-actions sticky-tabs-nav">
-			<RecordContext.Provider value={record}>
-				<Edit
-					resource="courses"
-					dataProviderName="power-course"
-					title={
-						<>
-							{record?.name}{' '}
-							<span className="text-gray-400 text-xs">#{record?.id}</span>
-						</>
-					}
-					headerButtons={() => null}
-					saveButtonProps={{
-						...saveButtonProps,
-						children: '儲存',
-						icon: null,
-						loading: mutation?.isLoading,
-					}}
-					footerButtons={({ defaultButtons }) =>
-						disableSaveButton ? null : (
-							<>
-								<Form {...formProps}>
-									<Item
-										noStyle
-										name={['status']}
-										getValueProps={(value) => {
-											return {
-												value: value === 'publish',
-											}
-										}}
-										normalize={(value) => {
-											return value ? 'publish' : 'draft'
-										}}
-									>
-										<Switch
-											className="mr-4"
-											checkedChildren="發佈"
-											unCheckedChildren="草稿"
-											disabled={disableSaveButton}
-										/>
-									</Item>
-								</Form>
-								{defaultButtons}
-							</>
-						)
-					}
-					isLoading={query?.isLoading}
-				>
-					<Tabs
-						activeKey={activeKey}
-						onChange={(key) => setActiveKey(key)}
-						items={items}
-						tabBarExtraContent={
-							<>
-								<Tooltip
-									title={
-										record?.classroom_link
-											? undefined
-											: '此課程還沒有章節，無法前往教室'
-									}
-								>
-									<Button
-										href={record?.classroom_link}
-										target="_blank"
-										rel="noreferrer"
-										className="ml-4"
-										type="default"
-										disabled={!record?.classroom_link}
-									>
-										前往教室
-									</Button>
-								</Tooltip>
+    return (
+        <div className="sticky-card-actions sticky-tabs-nav">
+            <RecordContext.Provider value={record}>
+                <Edit
+                    resource="courses"
+                    dataProviderName="power-course"
+                    title={
+                        <>
+                            {record?.name}{' '}
+                            <span className="text-gray-400 text-xs">#{record?.id}</span>
+                        </>
+                    }
+                    headerButtons={() => null}
+                    saveButtonProps={{
+                        ...saveButtonProps,
+                        children: '儲存',
+                        icon: null,
+                        loading: mutation?.isLoading,
+                    }}
+                    footerButtons={({defaultButtons}) =>
+                        disableSaveButton ? null : (
+                            <>
+                                <Form {...formProps}>
+                                    <Item
+                                        noStyle
+                                        name={['status']}
+                                        getValueProps={(value) => {
+                                            return {
+                                                value: value === 'publish',
+                                            }
+                                        }}
+                                        normalize={(value) => {
+                                            return value ? 'publish' : 'draft'
+                                        }}
+                                    >
+                                        <Switch
+                                            className="mr-4"
+                                            checkedChildren="發佈"
+                                            unCheckedChildren="草稿"
+                                            disabled={disableSaveButton}
+                                        />
+                                    </Item>
+                                </Form>
+                                {defaultButtons}
+                            </>
+                        )
+                    }
+                    isLoading={query?.isLoading}
+                >
+                    <Tabs
+                        activeKey={activeKey}
+                        onChange={(key) => setActiveKey(key)}
+                        items={items}
+                        tabBarExtraContent={
+                            <>
+                                <Button
+                                    className="ml-4"
+                                    type="default"
+                                    href={record?.edit_url}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    前往傳統商品編輯介面
+                                </Button>
+                                <Tooltip
+                                    title={
+                                        record?.classroom_link
+                                            ? undefined
+                                            : '此課程還沒有章節，無法前往教室'
+                                    }
+                                >
+                                    <Button
+                                        href={record?.classroom_link}
+                                        target="_blank"
+                                        rel="noreferrer"
+                                        className="ml-4"
+                                        type="default"
+                                        disabled={!record?.classroom_link}
+                                    >
+                                        前往教室
+                                    </Button>
+                                </Tooltip>
 
-								<Button
-									href={`${SITE_URL}/${COURSE_PERMALINK_STRUCTURE}/${record?.slug}`}
-									target="_blank"
-									rel="noreferrer"
-									className="ml-4"
-									type="default"
-								>
-									前往銷售頁
-								</Button>
-							</>
-						}
-					/>
-				</Edit>
-			</RecordContext.Provider>
-		</div>
-	)
+                                <Button
+                                    href={`${SITE_URL}/${COURSE_PERMALINK_STRUCTURE}/${record?.slug}`}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="ml-4"
+                                    type="default"
+                                >
+                                    前往銷售頁
+                                </Button>
+                            </>
+                        }
+                    />
+                </Edit>
+            </RecordContext.Provider>
+        </div>
+    )
 }
 
 export default memo(CoursesEdit)
--- js/src/pages/admin/Courses/List/types/index.ts
+++ js/src/pages/admin/Courses/List/types/index.ts
@@ -1,118 +1,119 @@
-import { TVideo } from '@/components/formItem/VideoInput/types'
+import {TVideo} from '@/components/formItem/VideoInput/types'
 import {
-	TTerm,
-	TStockStatus,
-	TProductType,
-	TProductAttribute,
-	TImage,
+    TTerm,
+    TStockStatus,
+    TProductType,
+    TProductAttribute,
+    TImage,
 } from '@/components/product/ProductTable/types'
 
 export * from './user'
 
 // List 只會拿基本的欄位
 export type TCourseBaseRecord = {
-	id: string
-	type: TProductType
-	name: string
-	slug: string
-	date_created: string
-	date_modified: string
-	status: string
-	featured: boolean
-	catalog_visibility: string
-	sku: string
-	menu_order: number
-	virtual: boolean
-	downloadable: boolean
-	permalink: string
-	custom_rating: number
-	extra_review_count: number
-	price_html: string
-	regular_price: string
-	sale_price: string
-	on_sale: boolean
-	date_on_sale_from: string | null
-	date_on_sale_to: string | null
-	total_sales: number
-	stock: number | null
-	stock_status: TStockStatus
-	manage_stock: boolean
-	stock_quantity: number | null
-	backorders: 'yes'
-	backorders_allowed: boolean
-	backordered: boolean
-	low_stock_amount: number | null
-	categories: TTerm[]
-	tags: TTerm[]
-	images: TImage[]
-	is_course: 'yes' | 'no' | ''
-	is_free: 'yes' | 'no' | ''
-	hours: number
-	course_schedule: number
-	course_hour: number
-	course_minute: number
-	course_length: number
-	classroom_link: string
+    id: string
+    type: TProductType
+    name: string
+    slug: string
+    date_created: string
+    date_modified: string
+    status: string
+    featured: boolean
+    catalog_visibility: string
+    sku: string
+    menu_order: number
+    virtual: boolean
+    downloadable: boolean
+    permalink: string
+    edit_url: string
+    custom_rating: number
+    extra_review_count: number
+    price_html: string
+    regular_price: string
+    sale_price: string
+    on_sale: boolean
+    date_on_sale_from: string | null
+    date_on_sale_to: string | null
+    total_sales: number
+    stock: number | null
+    stock_status: TStockStatus
+    manage_stock: boolean
+    stock_quantity: number | null
+    backorders: 'yes'
+    backorders_allowed: boolean
+    backordered: boolean
+    low_stock_amount: number | null
+    categories: TTerm[]
+    tags: TTerm[]
+    images: TImage[]
+    is_course: 'yes' | 'no' | ''
+    is_free: 'yes' | 'no' | ''
+    hours: number
+    course_schedule: number
+    course_hour: number
+    course_minute: number
+    course_length: number
+    classroom_link: string
 }
 
 export type TCoursesLimit = {
-	limit_type: 'unlimited' | 'fixed' | 'assigned' | 'follow_subscription'
-	limit_value: number | ''
-	limit_unit: 'second' | 'day' | 'month' | 'year' | ''
+    limit_type: 'unlimited' | 'fixed' | 'assigned' | 'follow_subscription'
+    limit_value: number | ''
+    limit_unit: 'second' | 'day' | 'month' | 'year' | ''
 }
 
 // Edit, Show, Create 會拿全部的欄位
 export type TCourseRecord = TCourseBaseRecord &
-	TCoursesLimit & {
-		purchase_note: string
-		description: string
-		short_description: string
-		upsell_ids: number[]
-		cross_sell_ids: number[]
-		attributes: TProductAttribute[]
-		chapters?: TChapterRecord[]
-		qa_list: {
-			question: string
-			answer: string
-		}[]
-		is_popular: 'yes' | 'no' | ''
-		is_featured: 'yes' | 'no' | ''
-		show_review: 'yes' | 'no' | ''
-		reviews_allowed: boolean
-		show_review_tab: 'yes' | 'no' | ''
-		show_review_list: 'yes' | 'no' | ''
-		show_total_student: 'yes' | 'no' | ''
-		enable_comment: 'yes' | 'no' | ''
-		hide_single_course: 'yes' | 'no' | ''
-		extra_student_count: number
-		feature_video: TVideo
-		trial_video: TVideo
-		editor: 'power-editor' | 'elementor' | ''
-	}
+    TCoursesLimit & {
+    purchase_note: string
+    description: string
+    short_description: string
+    upsell_ids: number[]
+    cross_sell_ids: number[]
+    attributes: TProductAttribute[]
+    chapters?: TChapterRecord[]
+    qa_list: {
+        question: string
+        answer: string
+    }[]
+    is_popular: 'yes' | 'no' | ''
+    is_featured: 'yes' | 'no' | ''
+    show_review: 'yes' | 'no' | ''
+    reviews_allowed: boolean
+    show_review_tab: 'yes' | 'no' | ''
+    show_review_list: 'yes' | 'no' | ''
+    show_total_student: 'yes' | 'no' | ''
+    enable_comment: 'yes' | 'no' | ''
+    hide_single_course: 'yes' | 'no' | ''
+    extra_student_count: number
+    feature_video: TVideo
+    trial_video: TVideo
+    editor: 'power-editor' | 'elementor' | ''
+}
 
 export type TChapterRecord = {
-	id: string
-	type: 'chapter'
-	status: string
-	depth: number
-	name: string
-	slug?: string
-	date_created?: string
-	date_modified?: string
-	catalog_visibility?: string
-	description?: string
-	short_description?: string
-	sku?: undefined
-	menu_order?: number
-	total_sales?: undefined
-	permalink?: string
-	chapter_length: number
-	category_ids?: string[]
-	tag_ids?: string[]
-	images?: TImage[]
-	chapters?: TChapterRecord[]
-	parent_id?: string
-	chapter_video?: TVideo
-	enable_comment: 'yes' | 'no'
-	editor: 'power-editor' | 'elementor' | ''
+    id: string
+    type: 'chapter'
+    status: string
+    depth: number
+    name: string
+    slug?: string
+    date_created?: string
+    date_modified?: string
+    catalog_visibility?: string
+    description?: string
+    short_description?: string
+    sku?: undefined
+    menu_order?: number
+    total_sales?: undefined
+    permalink?: string
+    chapter_length: number
+    category_ids?: string[]
+    tag_ids?: string[]
+    images?: TImage[]
+    chapters?: TChapterRecord[]
+    parent_id?: string
+    chapter_video?: TVideo
+    enable_comment: 'yes' | 'no'
+    editor: 'power-editor' | 'elementor' | ''
 }
