58f05415aec275ba7fc305b0af1b6efb478298d9
J7 <j7.dev.gg@gmail.com>
2026-03-10 19:07:00 +0800

    Revert "Merge pull request #101 from j7-dev/copilot/implement-auto-grant-courses-logic"
    This reverts commit fae67900ed4286cb2798295ded80aa6f6eaa82a7, reversing
    changes made to fe00c2a39324c178f86943719c64e83e845b4e9d.

--- inc/classes/Bootstrap.php
+++ inc/classes/Bootstrap.php
@@ -26,7 +26,6 @@ final class Bootstrap {
 		Compatibility\Compatibility::instance();
 
 		Resources\Loader::instance();
-		Resources\Course\AutoGrant::instance();
 
 		Admin\Entry::instance();
 		Admin\Product::instance();
